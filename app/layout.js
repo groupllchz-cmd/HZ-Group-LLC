8697993aade4e82aa5f69ba3138da7b2e6d65564
Beenish Hanif <beenishhanif11@gmail.com>
2026-01-29 19:33:36 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata = {
@@ -44,7 +39,7 @@ export const metadata = {
     siteName: "HZ Group LLC",
     images: [
       {
-        url: "/images/logo.png", // upload a company/branding image here
+        url: "/images/logo.png",
         width: 1200,
         height: 630,
         alt: "HZ Group LLC Technology Services",
@@ -55,15 +50,11 @@ export const metadata = {
   },
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <AppRouterCacheProvider
-          options={{ key: 'css' }}>
+      <body className={`${inter.variable} antialiased`}>
+        <AppRouterCacheProvider options={{ key: "css" }}>
           <Navbar />
           {children}
           <Footer />
