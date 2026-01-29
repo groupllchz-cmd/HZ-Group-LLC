3eaf868b557bbda29bdf9566126426f6c3ad886d
Beenish Hanif <beenishhanif11@gmail.com>
2026-01-29 19:34:52 +0500

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';  
 import About from '@/components/About'
 import ContactBanner from '@/components/ContactBanner'
 import Hero from '@/components/Hero'
@@ -6,11 +5,6 @@ import React from 'react'
 
 const page = () => {
   return (
-     <Head>
-  <title>About HZ Group LLC | Software Development & Web Design Agency</title>
-  <meta name="description" content="HZ Group LLC is a trusted software development agency specializing in custom web development, IT solutions, cloud services, and digital transformation for businesses worldwide." />
-  <meta name="keywords" content="software development agency, custom software solutions, web development company, IT consulting services, technology innovation partner, cloud solutions, digital transformation, Developers, enterprise software" />
-</Head>
     <div>
       <Hero
         title="About Us"
